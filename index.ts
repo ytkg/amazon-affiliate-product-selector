f78d10e4a33e852529e5ba755fd7665afa49202a
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2023-01-25 01:07:41 +0900

    Use Validator Middleware

--- index.ts
+++ index.ts
@@ -1,20 +1,25 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { validator } from 'hono/validator'
 import pickAdTags from './lib/pick_ad_tags'
 
 const app = new Hono()
 
 app.use('/', cors())
-app.get('/', async (c) => {
-  if (c.req.query('tags') === undefined) {
-    return c.text('tags is a required parameter.')
+app.get(
+  '/',
+  validator((v) => ({
+    tags: v.query('tags').isRequired().message('tags is a required parameter.'),
+    count: v.query('count').isOptional().isNumeric().message('count must be numeric.'),
+  })),
+  async (c) => {
+    const params = c.req.valid()
+    const tags = params.tags.split(',')
+    const count = parseInt(params.count)
+    const adTags = pickAdTags(tags, count)
+
+    return c.json(adTags)
   }
-
-  const tags = c.req.query('tags').split(',')
-  const count = parseInt(c.req.query('count'))
-  const adTags = pickAdTags(tags, count)
-
-  return c.json(adTags)
-})
+)
 
 export default app
